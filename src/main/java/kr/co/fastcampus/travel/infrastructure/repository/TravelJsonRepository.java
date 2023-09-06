782e45b855ec96cfe6cd09edae032457a990c461
cyPark95 <parkcy1007@gmail.com>
2023-09-06 14:21:59 +0900

    refactor: Json 파일 관련 로직 분리
    - Json 관련 로직 TravelJsonRepository 클래스로 분리
    - 파일 입출력 관련 로직 FileIORepository 추상 클래스로 분리

--- src/main/java/kr/co/fastcampus/travel/infrastructure/repository/TravelJsonRepository.java
+++ src/main/java/kr/co/fastcampus/travel/infrastructure/repository/TravelJsonRepository.java
@@ -1,5 +1,53 @@
 package kr.co.fastcampus.travel.infrastructure.repository;
 
-public class TravelJsonRepository {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.IOException;
+import kr.co.fastcampus.travel.domain.Trip;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+public class TravelJsonRepository extends FileIORepository {
+
+    private static final String ROOT_PATH = "travel/json";
+    private static final String TRIP_LIST_FILENAME = ROOT_PATH + "/trips";
+    private static final String EXTENSION = ".json";
+
+    private final ObjectMapper objectMapper;
+
+    public void saveTripFile(Trip trip) {
+        String filename = TRIP_LIST_FILENAME + EXTENSION;
+        ArrayNode jsonTrip = getJsonTrip(filename, trip);
+        writeFile(filename, jsonTrip.toPrettyString());
+    }
+
+    private ArrayNode parseJsonArray(String filename) {
+        String content = readFile(filename);
+        try {
+            JsonNode readJson = objectMapper.readTree(content);
+            if (readJson != null && readJson.isArray()) {
+                return (ArrayNode) readJson;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return objectMapper.createArrayNode();
+    }
+
+    private ArrayNode getJsonTrip(String filename, Trip trip) {
+        ArrayNode jsonTrips = parseJsonArray(filename);
+        jsonTrips.add(parserJsonTrip(trip));
+        return jsonTrips;
+    }
+
+    private ObjectNode parserJsonTrip(Trip trip) {
+        ObjectNode jsonTrip = objectMapper.createObjectNode();
+        jsonTrip.put("id", trip.getId());
+        jsonTrip.put("name", trip.getName());
+        jsonTrip.put("startAt", trip.getStartAt().toString());
+        jsonTrip.put("endAt", trip.getEndAt().toString());
+        return jsonTrip;
+    }
 }
