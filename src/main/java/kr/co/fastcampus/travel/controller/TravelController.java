a5271c5e31b977ce9b5cd4e8fbea34b5885e9eaa
hyeond0 <a98792530@gmail.com>
2023-09-07 12:04:04 +0900

    feat: 하나의 여정 상세 보기 기능 구현
    - Itinerary를 optional로 받아와서 값이 존재하지 않는다면 controller로 null 리턴
    - controller에서 null이라면 id에 해당하는 여정이 없는 상태이므로 예외처리
    - 받아온 값을 DTO 변환 후 return

--- src/main/java/kr/co/fastcampus/travel/controller/TravelController.java
+++ src/main/java/kr/co/fastcampus/travel/controller/TravelController.java
@@ -1,32 +1,60 @@
 package kr.co.fastcampus.travel.controller;
 
-import kr.co.fastcampus.travel.controller.dto.*;
-
 import java.util.List;
+import kr.co.fastcampus.travel.common.exception.TravelDoesNotExistException;
+import kr.co.fastcampus.travel.controller.dto.ItineraryResponse;
+import kr.co.fastcampus.travel.controller.dto.ItinerarySaveRequest;
+import kr.co.fastcampus.travel.controller.dto.TripInfoResponse;
+import kr.co.fastcampus.travel.controller.dto.TripResponse;
+import kr.co.fastcampus.travel.controller.dto.TripSaveRequest;
+import kr.co.fastcampus.travel.domain.FileType;
+import kr.co.fastcampus.travel.domain.Itinerary;
+import kr.co.fastcampus.travel.service.ItineraryService;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class TravelController {
 
-    public List<TripInfoResponse> getTripList() {
-        return null;
-    }
+	private final ItineraryService itineraryService;
 
-    public TripResponse findTrip(Long id) {
-        return null;
-    }
+	public List<TripInfoResponse> getTripList() {
+		return null;
+	}
 
-    public String saveTrips(TripSaveRequest saveRequest) {
-        return null;
-    }
+	public TripResponse findTrip(Long id) {
+		return null;
+	}
 
-    public List<ItineraryResponse> getItineraryList(Long tripId) {
-        return null;
-    }
+	public String saveTrips(TripSaveRequest saveRequest) {
+		return null;
+	}
 
-    public ItineraryResponse findItinerary(Long id) {
-        return null;
-    }
+	public List<ItineraryResponse> getItineraryList(Long tripId) {
+		return null;
+	}
 
-    public String saveItineraries(Long tripId, List<ItinerarySaveRequest> saveRequests) {
-        return null;
-    }
+	public ItineraryResponse findItinerary(FileType fileType, Long id) {
+		// TOOD
+		// fileType, id의 valid check
+
+		Itinerary response = itineraryService.findItinerary(fileType, id);
+		if (response == null) {
+			throw new TravelDoesNotExistException("해당 id의 여정이 존재하지 않습니다.");
+		}
+
+		return ItineraryResponse.builder()
+			.id(response.getId())
+			.departure(response.getRoute().getDeparture())
+			.destination(response.getRoute().getDestination())
+			.departureAt(response.getRoute().getDepartureAt())
+			.arriveAt(response.getRoute().getArriveAt())
+			.accommodation(response.getLodge().getAccommodation())
+			.checkInAt(response.getLodge().getCheckInAt())
+			.checkOutAt(response.getLodge().getCheckOutAt())
+			.build();
+	}
+
+	public String saveItineraries(Long tripId, List<ItinerarySaveRequest> saveRequests) {
+		return null;
+	}
 }
--- src/main/java/kr/co/fastcampus/travel/service/ItineraryService.java
+++ src/main/java/kr/co/fastcampus/travel/service/ItineraryService.java
@@ -2,9 +2,9 @@ package kr.co.fastcampus.travel.service;
 
 import java.util.List;
 import java.util.Optional;
-import kr.co.fastcampus.travel.controller.dto.ItinerarySaveRequest;
 import kr.co.fastcampus.travel.domain.FileType;
 import kr.co.fastcampus.travel.domain.Itinerary;
+import kr.co.fastcampus.travel.domain.Trip;
 import kr.co.fastcampus.travel.infrastructure.repository.ItineraryRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -16,28 +16,17 @@ public class ItineraryService {
 	public Itinerary findItinerary(FileType fileType, Long id) {
 		Optional<Itinerary> response = itineraryRepository.findById(fileType, id);
 		if (response.isPresent()) {
-			Itinerary itineraryEntity = response.get();
-			return Itinerary.builder()
-				.id(itineraryEntity.getId())
-				.departure(itineraryEntity.getRoute().getDeparture())
-				.destination(itineraryEntity.getRoute().getDestination())
-				.departureAt(itineraryEntity.getRoute().getDepartureAt())
-				.arriveAt(itineraryEntity.getRoute().getArriveAt())
-				.accommodation(itineraryEntity.getLodge().getAccommodation())
-				.checkInAt(itineraryEntity.getLodge().getCheckInAt())
-				.checkOutAt(itineraryEntity.getLodge().getCheckOutAt())
-				.build();
+			return response.get();
 		} else {
 			return null;
 		}
 	}
 
-	public List<Itinerary> findItineraries(Long tripId) {
+	public List<Itinerary> findItineraries(FileType fileType, Trip trip) {
 		return null;
 	}
 
-	public List<Itinerary> saveItineraries(Long tripId,
-		List<ItinerarySaveRequest> itinerarySaveRequests) {
+	public List<Itinerary> saveItineraries(Long tripId) {
 		return null;
 	}
 }
