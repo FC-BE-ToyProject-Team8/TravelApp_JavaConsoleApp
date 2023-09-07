9cbfc87f8b446a24a8d60ff4392cd67d3e7b06fe
deepredk <deepred.kim@gmail.com>
2023-09-07 20:32:23 +0900

    refactor: Menu Enum 생성자/Getter에 대해 롬복 사용하도록 수정함

--- src/main/java/kr/co/fastcampus/travel/view/Menu.java
+++ src/main/java/kr/co/fastcampus/travel/view/Menu.java
@@ -2,15 +2,15 @@ package kr.co.fastcampus.travel.view;
 
 import java.util.Arrays;
 import java.util.NoSuchElementException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public enum Menu {
     LOG_TRIP(1), LOG_ITINERARY(2), SHOW_TRIP(3), SHOW_ITINERARY(4), EXIT(5);
 
-    private int number;
-
-    Menu(int number) {
-        this.number = number;
-    }
+    private final int number;
 
     public static Menu fromNumber(int number) {
         try {
@@ -20,7 +20,4 @@ public enum Menu {
         }
     }
 
-    public int getNumber() {
-        return number;
-    }
 }
