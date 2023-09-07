577bf012f8df2ea031e560b5288befb93c498202
deepredk <deepred.kim@gmail.com>
2023-09-07 20:50:05 +0900

    refactor: TravelDoesNotExistException 불필요한 생성자 제거
    심각한 오류라기 보다는 사용자에게 예외전달만 해줘도 될것 같아서 ErrorCode만 활용해도 충분할 것 같아 아래 생성자 2개 삭제
    - constructor(String message)
    - constructor(String message, Throwable cause)

--- src/main/java/kr/co/fastcampus/travel/common/exception/TravelDoesNotExistException.java
+++ src/main/java/kr/co/fastcampus/travel/common/exception/TravelDoesNotExistException.java
@@ -5,12 +5,4 @@ public class TravelDoesNotExistException extends BaseException {
     public TravelDoesNotExistException() {
         super(ErrorCode.TRAVEL_DOES_NOT_EXIST);
     }
-
-    public TravelDoesNotExistException(String message) {
-        super(message, ErrorCode.TRAVEL_DOES_NOT_EXIST);
-    }
-
-    public TravelDoesNotExistException(String message, Throwable cause) {
-        super(message, ErrorCode.TRAVEL_DOES_NOT_EXIST, cause);
-    }
 }
