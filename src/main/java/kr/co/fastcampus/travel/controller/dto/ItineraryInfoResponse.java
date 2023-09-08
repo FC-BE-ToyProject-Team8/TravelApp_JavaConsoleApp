a343235ca80bb94bfd343aabeb5fb854675fa878
hyeond0 <a98792530@gmail.com>
2023-09-08 19:36:21 +0900

    refactor: 여정 조회 콘솔 기능 수정, 코드 컨벤션 수정
    - 여정 조회 toString에서 콘솔 창에서 출력으로 수정
    - printItinerary 메소드를 만들어 여정 상세 목록
    출력
    - DTO 들여쓰기 및 개행 수정

--- src/main/java/kr/co/fastcampus/travel/controller/dto/ItineraryInfoResponse.java
+++ src/main/java/kr/co/fastcampus/travel/controller/dto/ItineraryInfoResponse.java
@@ -2,11 +2,14 @@ package kr.co.fastcampus.travel.controller.dto;
 
 import kr.co.fastcampus.travel.domain.Itinerary;
 
-public record ItineraryInfoResponse(Long id, String departure, String destination) {
+public record ItineraryInfoResponse(Long id,
+									String departure,
+									String destination
+) {
 
 	public ItineraryInfoResponse(Itinerary itinerary) {
 		this(itinerary.getId(),
-			itinerary.getRoute().getDeparture(),
-			itinerary.getRoute().getDestination());
+			 itinerary.getRoute().getDeparture(),
+			 itinerary.getRoute().getDestination());
 	}
 }
--- src/main/java/kr/co/fastcampus/travel/view/ConsoleView.java
+++ src/main/java/kr/co/fastcampus/travel/view/ConsoleView.java
@@ -1,15 +1,15 @@
 package kr.co.fastcampus.travel.view;
 
-import static kr.co.fastcampus.travel.common.exception.ErrorCode.TRAVEL_DOES_NOT_EXIST;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import kr.co.fastcampus.travel.common.exception.TravelDoesNotExistException;
 import kr.co.fastcampus.travel.common.exception.UnknownException;
 import java.util.List;
-import kr.co.fastcampus.travel.common.exception.BaseException;
 import kr.co.fastcampus.travel.controller.TravelController;
 import kr.co.fastcampus.travel.controller.dto.ItineraryInfoResponse;
 import kr.co.fastcampus.travel.controller.dto.ItineraryResponse;
@@ -20,110 +20,128 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ConsoleView {
 
-	private final TravelController travelController;
+    private final TravelController travelController;
 
-	private final BufferedReader br;
-	private boolean isExited = false;
+    private final BufferedReader br;
+    private boolean isExited = false;
 
-	public ConsoleView() {
-		br = new BufferedReader(new InputStreamReader(System.in));
-	}
+    public ConsoleView() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        travelController = new TravelController();
+    }
 
-	public void process() {
-		System.out.println("[메뉴]");
-		System.out.println(Arrays.stream(Menu.values())
-			.map(menu -> String.format("%d: %s", menu.getNumber(), menu.getName()))
-			.collect(Collectors.joining(", ")));
+    public void process() {
+        System.out.println("[메뉴]");
+        System.out.println(Arrays.stream(Menu.values())
+                .map(menu -> String.format("%d: %s", menu.getNumber(), menu.getName()))
+                .collect(Collectors.joining(", ")));
 
-		System.out.println("\n메뉴 번호를 입력해주세요");
+        System.out.println("\n메뉴 번호를 입력해주세요");
 
-		Menu menu = inputMenu();
-		if (menu == Menu.LOG_TRIP) {
-			logTrip();
-		} else if (menu == Menu.SHOW_ITINERARY) {
-			showItinerary();
-		}
+        Menu menu = inputMenu();
+        if (menu == Menu.LOG_TRIP) {
+            logTrip();
+        } else if (menu == Menu.SHOW_ITINERARY) {
+            showItinerary();
+        }
 
-		isExited = true;
-	}
+        isExited = true;
+    }
 
-	private void logTrip() {
-		System.out.println("여행 기록을 시작합니다.");
+    private void logTrip() {
+        System.out.println("여행 기록을 시작합니다.");
 
-	}
+    }
 
-	private Menu inputMenu() {
-		try {
-			int menuNumber = inputNumber("잘못된 메뉴 번호입니다. 다시 입력해주세요;");
-			return Menu.fromNumber(menuNumber);
-		} catch (IllegalArgumentException e) {
-			return inputMenu();
-		}
-	}
+    private Menu inputMenu() {
+        try {
+            int menuNumber = inputNumber("잘못된 메뉴 번호입니다. 다시 입력해주세요;");
+            return Menu.fromNumber(menuNumber);
+        } catch (IllegalArgumentException e) {
+            return inputMenu();
+        }
+    }
 
-	private void showItinerary() {
-		List<TripInfoResponse> trips = travelController.getTripList();
-		if (trips.isEmpty()) {
-			System.out.println("여행 목록이 비어 있습니다.");
-			throw new BaseException(TRAVEL_DOES_NOT_EXIST);
-		}
-		System.out.println("여정 조회를 시작합니다.");
-		for (TripInfoResponse tripInfo : trips) {
-			System.out.println(tripInfo);
-		}
-		System.out.println("조회할 여행의 번호를 입력하세요.");
-		Long tripNum = (long) inputNumber("잘못된 여행 번호입니다. 다시 입력해주세요")
-		System.out.print("조회할 여행의 데이터 타입을 입력하세요. (1. CSV, 2. JSON) : ");
-		FileType fileType = inputFileType();
-		List<ItineraryInfoResponse> itineraries = travelController.getItineraryList(fileType,
-			tripNum);
-		System.out.println("여정 목록");
-		for (ItineraryInfoResponse itineraryInfo : itineraries) {
-			System.out.println(itineraryInfo);
-		}
-		System.out.print("조회할 여정의 번호를 입력해주세요. : ");
-		Long itineraryNum = (long) inputNumber("잘못된 여정 번호입니다. 다시 입력해주세요")
-		System.out.print("조회할 여정의 데이터 타입을 입력하세요. (1. CSV, 2. JSON) : ");
-		fileType = inputFileType();
-		ItineraryResponse itineraryResponse = travelController.findItinerary(fileType,
-			itineraryNum);
-		System.out.println(itineraryResponse);
-		System.out.println("Enter를 누르면 시작 메뉴로 돌아갑니다.");
-		readLine();
-	}
+    private void showItinerary() {
+        List<TripInfoResponse> trips = travelController.getTripList();
+        if (trips.isEmpty()) {
+            System.out.println("여행 목록이 비어 있습니다.");
+            throw new TravelDoesNotExistException();
+        }
+        System.out.println("여정 조회를 시작합니다.");
+        for (TripInfoResponse tripInfo : trips) {
+            System.out.println(String.format("%d: %s (%s ~ %s)",
+                    tripInfo.id(), tripInfo.name(), tripInfo.startAt(), tripInfo.endAt()));
+        }
+        System.out.println("조회할 여행의 번호를 입력하세요.");
+        Long tripNum = (long) inputNumber("잘못된 여행 번호입니다. 다시 입력해주세요");
+        System.out.print("조회할 여행의 데이터 타입을 입력하세요. (1. CSV, 2. JSON) : ");
+        FileType fileType = inputFileType();
+        List<ItineraryInfoResponse> itineraries = travelController.getItineraryList(fileType,
+                tripNum);
+        System.out.println("여정 목록");
+        for (ItineraryInfoResponse itineraryInfo : itineraries) {
+            System.out.println(
+                    String.format("%d: %s ~ %s",
+                            itineraryInfo.id(), itineraryInfo.departure(), itineraryInfo.destination()));
+        }
+        System.out.print("조회할 여정의 번호를 입력해주세요. : ");
+        Long itineraryNum = (long) inputNumber("잘못된 여정 번호입니다. 다시 입력해주세요");
+        System.out.print("조회할 여정의 데이터 타입을 입력하세요. (1. CSV, 2. JSON) : ");
+        fileType = inputFileType();
+        ItineraryResponse itineraryResponse = travelController.findItinerary(fileType,
+                itineraryNum);
+        printItineraryDetail(itineraryResponse);
+    }
 
-	private FileType inputFileType() {
-	}
+    private void printItineraryDetail(ItineraryResponse itineraryResponse) {
+        DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        StringBuilder sb = new StringBuilder();
+        sb.append("출발 : ").append(itineraryResponse.departure()).append(", ")
+                .append(formatterDate.format(itineraryResponse.departureAt())).append("\n");
+        sb.append("도착 : ").append(itineraryResponse.destination())
+                .append(formatterDate.format(itineraryResponse.departureAt())).append("\n");
+        sb.append("숙박 시설: ").append(itineraryResponse.accommodation()).append("\n");
+        sb.append("체크인: ").append(formatterTime.format(itineraryResponse.checkInAt())).append("\n");
+        sb.append("체크아웃: ").append(formatterTime.format(itineraryResponse.checkOutAt()))
+                .append("\n");
+        System.out.println(sb);
+    }
 
-	private int inputNumber(String errorMessage) {
-		while (true) {
-			try {
-				return parseInt(readLine());
-			} catch (IllegalArgumentException e) {
-				System.out.println(errorMessage);
-			}
-		}
-	}
+    private FileType inputFileType() {
+        return null;
+    }
 
-	private int parseInt(String strNum) {
-		try {
-			return Integer.parseInt(strNum);
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException();
-		}
-	}
+    private int inputNumber(String errorMessage) {
+        while (true) {
+            try {
+                return parseInt(readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(errorMessage);
+            }
+        }
+    }
 
-	private String readLine() {
-		try {
-			return br.readLine();
-		} catch (IOException e) {
-			System.out.println("입력을 받는 도중 알 수 없는 에러가 발생했습니다.");
-			throw new UnknownException();
-		}
-	}
+    private int parseInt(String strNum) {
+        try {
+            return Integer.parseInt(strNum);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
 
-	public boolean isExited() {
-		return isExited;
-	}
+    private String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            System.out.println("입력을 받는 도중 알 수 없는 에러가 발생했습니다.");
+            throw new UnknownException();
+        }
+    }
+
+    public boolean isExited() {
+        return isExited;
+    }
 }
 
