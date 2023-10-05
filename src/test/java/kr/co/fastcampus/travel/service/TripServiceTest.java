542a718ed3d3419118470f7423a1f3e783512a89
cyPark95 <parkcy1007@gmail.com>
2023-10-05 11:41:25 +0900

    test: 여행 서비스 테스트
    - 여행 저장 확인
    - 모든 여행 조회(결과 유/무) 확인
    - 여행 단건 조회(결과 유/무) 확인

---
+++ src/test/java/kr/co/fastcampus/travel/service/TripServiceTest.java
@@ -0,0 +1,113 @@
+package kr.co.fastcampus.travel.service;
+
+import static kr.co.fastcampus.travel.view.enums.FileType.CSV;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.IntStream;
+import kr.co.fastcampus.travel.common.exception.TravelDoesNotExistException;
+import kr.co.fastcampus.travel.controller.dto.TripSaveRequest;
+import kr.co.fastcampus.travel.domain.Trip;
+import kr.co.fastcampus.travel.mock.FakeTripRepository;
+import kr.co.fastcampus.travel.repository.TripRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("여행 서비스 테스트")
+class TripServiceTest {
+
+    public static final String TRIP_NAME = "여행";
+    public static final LocalDate TODAY = LocalDate.now();
+
+    private TripRepository tripRepository;
+    private TripService tripService;
+
+    @BeforeEach
+    void setup() {
+        tripRepository = new FakeTripRepository();
+        tripService = new TripService(tripRepository);
+    }
+
+    @Test
+    @DisplayName("여행 저장")
+    void saveTrip() {
+        // given
+        TripSaveRequest request = TripSaveRequest.builder()
+                .name(TRIP_NAME)
+                .startAt(TODAY)
+                .endAt(TODAY.plusDays(3))
+                .build();
+
+        // when
+        Trip result = tripService.saveTrip(request);
+
+        // then
+        assertEquals(0L, result.getId());
+    }
+
+    @Test
+    @DisplayName("모든 여행 조회")
+    void findAllTrips() {
+        // given
+        IntStream.range(0, 3)
+                .forEach(this::saveTrip);
+
+        // when
+        List<Trip> result = tripService.findAllTrips(CSV);
+
+        // then
+        assertEquals(3, result.size());
+        IntStream.range(0, 3)
+                .forEach(i -> assertEqualsTrip(i, result.get(i)));
+    }
+
+    @Test
+    @DisplayName("여행 단건 조회 - 결과 없음")
+    void findTrip_notExist() {
+        // when
+        // then
+        assertThrows(
+                TravelDoesNotExistException.class,
+                () -> tripService.findTrip(CSV, 0L)
+        );
+    }
+
+    @Test
+    @DisplayName("여행 단건 조회")
+    void findTrip() {
+        // given
+        saveTrip(0);
+
+        // when
+        Trip result = tripService.findTrip(CSV, 0L);
+
+        // then
+        assertEqualsTrip(0, result);
+    }
+
+    private void assertEqualsTrip(int index, Trip result) {
+        assertAll(
+                () -> assertEquals(index, result.getId()),
+                () -> assertEquals(TRIP_NAME + index, result.getName()),
+                () -> assertEquals(TODAY.plusDays(index), result.getStartAt()),
+                () -> assertEquals(TODAY.plusDays(index + 3), result.getEndAt())
+        );
+    }
+
+    private void saveTrip(int index) {
+        Trip trip = createTrip(index);
+        tripRepository.save(trip);
+    }
+
+    private Trip createTrip(int index) {
+        return Trip.builder()
+                .name(TRIP_NAME + index)
+                .startAt(TODAY.plusDays(index))
+                .endAt(TODAY.plusDays(index + 3))
+                .build();
+    }
+}
